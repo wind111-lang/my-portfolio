9f761347e7e9ad5d569720c3fd50c291d4dccf61
wind111-lang <shota.tsutsui@prtimes.co.jp>
2025-03-11 17:45:20 +0900

    blog added

--- app/components/blog_section.tsx
+++ app/components/blog_section.tsx
@@ -8,25 +8,56 @@ export default function BlogSection(): React.ReactNode {
       </h2>
       <article className="mb-6">
         <a
-          href="https://yahoo.co.jp"
-          className="hover:text-blue-500 dark:hover:text-blue-500 text-gray-800 dark:text-gray-200"
-        >
-          <h3 className="text-2xl font-bold">記事タイトル例</h3>
-          <p className="text-lg">この記事の概要をここに記述します…</p>
-        </a>
-        <a
-          href="https://yahoo.co.jp"
+          href="https://qiita.com/wind111-lang/items/af7e3fadeb1c71673cda"
           className="hover:text-blue-500 dark:hover:text-blue-500  text-gray-800 dark:text-gray-200"
         >
-          <h3 className="text-2xl font-bold">記事タイトル例</h3>
-          <p className="text-lg">この記事の概要をここに記述します…</p>
+          <h3 className="text-2xl font-bold">
+            GoオンリーでGUI上からQRコード読み取りをしてみた
+          </h3>
+          <p className="text-lg">
+            GoでOpenCVとQtを利用して、QRコードを読み取る方法について紹介した記事です。初執筆。
+          </p>
         </a>
         <a
-          href="https://yahoo.co.jp"
+          href="https://zenn.dev/wind111/articles/4cc15edad10508"
           className="hover:text-blue-500 dark:hover:text-blue-500 text-gray-800 dark:text-gray-200"
         >
-          <h3 className="text-2xl font-bold">記事タイトル例</h3>
-          <p className="text-lg">この記事の概要をここに記述します…</p>
+          <h3 className="text-2xl font-bold">Pythonで簡単に経路探索をする</h3>
+          <p className="text-lg">
+            経路探索パッケージ OSMnx
+            で経路を探索・可視化を行う方法について紹介した記事です。
+          </p>
+        </a>
+        <a
+          href="https://developers.prtimes.jp/2024/09/02/execute-bg-data-transfers/"
+          className="hover:text-blue-500 dark:hover:text-blue-500  text-gray-800 dark:text-gray-200"
+        >
+          <h3 className="text-2xl font-bold">
+            BigQuery Data Transfersをエンジニア全員に対して実行可能にした話
+          </h3>
+          <p className="text-lg">
+            Data Transfersをエンジニアが利用可能にするためのTipsです。
+          </p>
+        </a>
+        <a
+          href="https://qiita.com/wind111-lang/items/a93e243ed2e359ec30cc"
+          className="hover:text-blue-500 dark:hover:text-blue-500 text-gray-800 dark:text-gray-200"
+        >
+          <h3 className="text-2xl font-bold">PHPUnitでよく使うassertion</h3>
+          <p className="text-lg">
+            PHPのテストライブラリである PHPUnit でよく使用する
+            assertionをまとめた記事です。
+          </p>
+        </a>
+        <a
+          href="https://developers.prtimes.jp/2025/02/18/mock-aws-sdk-in-php/"
+          className="hover:text-blue-500 dark:hover:text-blue-500 text-gray-800 dark:text-gray-200"
+        >
+          <h3 className="text-2xl font-bold">PHPでAWS SDKのテストをMockする</h3>
+          <p className="text-lg">
+            PHPのモックライブラリである Mockery を利用してAWS
+            S3のテストをMockする方法について紹介した記事です。
+          </p>
         </a>
       </article>
     </section>
